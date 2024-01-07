9d974d6ddb2a499b08571fc3dcd76f159314b7e3
kokito741 <48101187+kokito741@users.noreply.github.com>
2024-01-07 05:50:51 +0200

    added histograps

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -20,20 +20,290 @@ function setupUI(user) {
 
     // get user UID to get data from database
     var uid = user.uid;
-    console.log(uid);
     prevHum="unkown";
     prevTemp="unkown";
 //todo make the room,device and date dinamicly chosen
     // Database paths (with user UID)
     updateReadings();
+    drawHistogram_PER_HOUR(uid);
+    drawHistogram_PER_DAY(uid);
+    drawHistogram_PER_MONTH(uid);
+
+    function drawHistogram_PER_HOUR(uid) {
+      // Define the start and end times
+      var startTime = moment().startOf('day'); // This sets the start time to the beginning of today
+      var endTime = moment(); // This sets the end time to the current time
+      var temperatures = [];
+      var humidities = [];
+      // Initialize empty arrays for temperature and humidity
+      var temperaturePromises = [];
+      var humidityPromises = [];
+      var date = moment().format('DD-MM-YYYY'); // Get the current date
+
+      // Loop through each hour between the start and end times
+      for (var m = moment(startTime); m.isBefore(endTime); m.add(1, 'hours')) {
+        // Format the current time
+        var hour = m.format('HH');
+        // Define the database paths
+        var dbPathTemp = uid.toString()+'/Average per hour/Living Room/'+date+ " - "+ hour + '/temperature';
+        var dbPathHum = uid.toString()+'/Average per hour/Living Room/'+date+ " - "+ hour + '/humidity';
+        // Get the hour from the database path
     
+        // Get the temperature and humidity data from the Firebase database
+        var dbRefTemp = firebase.database().ref().child(dbPathTemp);
+        var dbRefHum = firebase.database().ref().child(dbPathHum);
+
+        // Get the temperature 
+        
+        var tempPromise = ((hour) => {
+          return dbRefTemp.once('value').then(snap => {
+            var val = snap.val();
+            if (val !== null && val !== undefined) {
+              temperatures.push({hours: hour, value: val});
+            }
+          });
+        })(hour);
+        temperaturePromises.push(tempPromise);
+
+        // Get the humidity value
+        // Get the humidity value
+        var humPromise = ((hour) => {
+          return dbRefHum.once('value').then(snap => {
+            var val = snap.val();
+            if (val !== null && val !== undefined) {
+              humidities.push({hours: hour, value: val});
+            }
+          });
+        })(hour);
+        humidityPromises.push(humPromise);
+      }
+
+      // Wait for all promises to resolve, then draw the chart
+      Promise.all(temperaturePromises.concat(humidityPromises)).then(() => {
+        // After the loop, draw the histogram using the Chart.js library
+        var ctx = document.getElementById('myChart_PER_HOURS').getContext('2d');
+        var myChart_PER_HOURS = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: temperatures.map(item => item.hours), // Use the hours from the temperatures array as labels
+            datasets: [{
+              label: 'Temperature',
+              data: temperatures.map(item => item.value), // Use the values from the temperatures array as data
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1
+            }, {
+              label: 'Humidity',
+              data: humidities.map(item => item.value), // Use the values from the humidities array as data
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'HOURLY AVERAGE'
+                }
+              },
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+      });
+    }
+    
+    function drawHistogram_PER_DAY(uid) {
+      // Define the start and end times
+      var startTime = moment().startOf('month'); // This sets the start time to the beginning of the first day of the current month
+      var endTime = moment().startOf('day'); // This sets the end time to the start of today
+      var temperatures = [];
+      var humidities = [];
+      // Initialize empty arrays for temperature and humidity
+      var temperaturePromises = [];
+      var humidityPromises = [];
+
+      // Loop through each day between the start and end times
+      for (var m = moment(startTime); m.isBefore(endTime); m.add(1, 'days')) {
+        // Format the current time
+        var date = m.format('DD-MM-YYYY');
+        // Define the database paths
+        var dbPathTemp = uid.toString()+'/Average per day/Living Room/'+ date + '/temperature';
+        var dbPathHum = uid.toString()+'/Average per day/Living Room/'+date + '/humidity';
+        // Get the date from the database path
+        var dateTempSegment = dbPathTemp.split('/')[3];
+        var dateHumSegment = dbPathHum.split('/')[3];
+         // Corrected here
+        // Get the temperature and humidity data from the Firebase database
+        var dbRefTemp = firebase.database().ref().child(dbPathTemp);
+        var dbRefHum = firebase.database().ref().child(dbPathHum);
+
+        // Get the temperature value
+        var tempPromise = ((date) => {
+          return dbRefTemp.once('value').then(snap => {
+            var val = snap.val();
+            if (val !== null && val !== undefined) {
+              temperatures.push({date: date, value: val});
+            }
+          }).catch(error => {
+            console.error('Error reading data:', error);
+          });
+        })(date);
+        temperaturePromises.push(tempPromise);
+          
+        // Get the humidity value
+        // Get the humidity value
+        var humPromise = ((date) => {
+          return dbRefHum.once('value').then(snap => {
+            var val = snap.val();
+            if (val !== null && val !== undefined) {
+              humidities.push({date: date, value: val});
+            }
+          });
+        })(date);
+        humidityPromises.push(humPromise);
+      }
+
+      // Wait for all promises to resolve, then draw the chart
+      Promise.all(temperaturePromises.concat(humidityPromises)).then(() => {
+        // Create an array of days
+        var daysInMonth = moment().daysInMonth();
+        var days = Array.from({length: daysInMonth}, (_, i) => moment().startOf('month').add(i, 'days').format('DD-MM-YYYY'));
+        // After the loop, draw the histogram using the Chart.js library
+        var ctx = document.getElementById('myChart_PER_DAYS').getContext('2d');
+        console.log(temperatures);
+        var myChart_PER_DAYS = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: temperatures.map(item => item.date), // Use the dates from the temperatures array as labels
+            datasets: [{
+              label: 'Temperature',
+              data: temperatures.map(item => item.value), // Use the values from the temperatures array as data
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1
+            }, {
+              label: 'Humidity',
+              data: humidities.map(item => item.value), // Use the values from the humidities array as data
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'DAILY AVERAGE'
+                }
+              },
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+      });
+    }
+
+    function drawHistogram_PER_MONTH(uid) {
+      // Define the start and end times
+      var startTime = moment().subtract(1, 'years').startOf('year'); // This sets the start time to the beginning of the previous year
+      var endTime = moment(); // This sets the end time to the current time
+      var temperatures = [];
+      var humidities = [];
+      // Initialize empty arrays for temperature and humidity
+      var temperaturePromises = [];
+      var humidityPromises = [];
+
+      // Loop through each month between the start and end times
+      for (var m = moment(startTime); m.isBefore(endTime); m.add(1, 'months')) {
+        // Format the current month
+        var month = m.format('MM-YYYY');
+        // Define the database paths
+        var dbPathTemp = uid.toString()+'/Average per month/Living Room/'+ month + '/temperature';
+        var dbPathHum = uid.toString()+'/Average per month/Living Room/'+ month + '/humidity';
+        console.log(dbPathTemp);
+        // Get the temperature and humidity data from the Firebase database
+        var dbRefTemp = firebase.database().ref().child(dbPathTemp);
+        var dbRefHum = firebase.database().ref().child(dbPathHum);
+        console.log(dbRefTemp);
+        // Get the temperature 
+        var tempPromise = ((month) => {
+          return dbRefTemp.once('value').then(snap => {
+            var val = snap.val();
+            if (val !== null && val !== undefined) {
+              temperatures.push({month: month, value: val});
+              console.log(temperatures);
+            }
+          });
+        })(month);
+        temperaturePromises.push(tempPromise);
+
+        // Get the humidity value
+        var humPromise = ((month) => {
+          return dbRefHum.once('value').then(snap => {
+            var val = snap.val();
+            if (val !== null && val !== undefined) {
+              humidities.push({month: month, value: val});
+            }
+          });
+        })(month);
+        humidityPromises.push(humPromise);
+      }
+
+      // Wait for all promises to resolve, then draw the chart
+      Promise.all(temperaturePromises.concat(humidityPromises)).then(() => {
+        // After the loop, draw the histogram using the Chart.js library
+        // After the loop, draw the histogram using the Chart.js library
+        var ctx = document.getElementById('myChart_PER_MONTH').getContext('2d');
+        var myChart_PER_MONTH = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: temperatures.map(item => item.month), // Use the months from the temperatures array as labels
+            datasets: [{
+              label: 'Temperature',
+              data: temperatures.map(item => item.value), // Use the values from the temperatures array as data
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1
+            }, {
+              label: 'Humidity',
+              data: humidities.map(item => item.value), // Use the values from the humidities array as data
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              x: {
+                title: {
+                  display: true,
+                  text: 'MONTHLY AVERAGE'
+                }
+              },
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+        
+      });
+    }
+
+
         // Function to update page with new readings
     function updateReadings() {
         var dateTime = moment().format('D-M-YYYY - HH-mm'); 
         var dbPathTemp = uid.toString()+'/Living Room/'+ dateTime + '/temperature';
         var dbPathHum =  uid.toString()+'/Living Room/'+dateTime + '/humidity';
-        console.log(dbPathTemp);
-        console.log(dbPathHum);
     
         // Database references
         var dbRefTemp = firebase.database().ref().child(dbPathTemp);
@@ -46,7 +316,6 @@ function setupUI(user) {
             prevTemp = val; // Update previous value
             }
             tempElement.innerText = prevTemp; // Use previous value if no new value
-            console.log(prevTemp);
         });
 
         dbRefHum.once('value', snap => {
@@ -55,15 +324,17 @@ function setupUI(user) {
             prevHum = val; // Update previous value
             }
             humElement.innerText = prevHum; // Use previous value if no new value
-            console.log(prevHum);
         });
+
     }
+
 
     // Call updateReadings every 10seconds
     setInterval(updateReadings, 10000);
-
-
-   
+    // Call drawHistogram every 10 minutes
+    setInterval(drawHistogram_PER_HOUR, 10 * 60 * 1000);
+    setInterval(drawHistogram_PER_DAY, 10 * 60 * 1000);
+    setInterval(drawHistogram_PER_MONTH, 10 * 60 * 1000);
 
   // if user is logged out
   } else{
@@ -78,13 +349,9 @@ document.addEventListener("DOMContentLoaded", function(){
     // listen for auth status changes
     auth.onAuthStateChanged(user => {
      if (user) {
-       console.log("user logged in");
-       console.log(user);
        setupUI(user);
        var uid = user.uid;
-       console.log(uid);
      } else {
-       console.log("user logged out");
        setupUI();
      }
     });
@@ -100,13 +367,12 @@ document.addEventListener("DOMContentLoaded", function(){
      auth.signInWithEmailAndPassword(email, password).then((cred) => {
        // close the login modal & reset form
        loginForm.reset();
-       console.log(email);
      })
      .catch((error) =>{
        const errorCode = error.code;
        const errorMessage = error.message;
        document.getElementById("error-message").innerHTML = errorMessage;
-       console.log(errorMessage);
+       console.error(errorMessage);
      });
     });
     
